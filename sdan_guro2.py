6209f6f8e2b0a99207353697aa628e8a0ddfbbcf
datams <lr-movie@gmx.net>
2014-11-14 18:04:10 +0100

    graph, demand, result gurobi conversion works

--- sdan_guro2.py
+++ sdan_guro2.py
@@ -6,19 +6,18 @@ import demand as dem
 import customGraph as gc
 print chr(27) + "[2J"
 
-##### functions #####
-def new_request(s2set, t2set, bw2set, lat2set, s ,t ,bw_r ,lat_r ,Requests):
-	a=len(Requests)+1
-	Requests.append(a)
-	s[a]=s2set
-	t[a]=t2set
-	bw_r[a]=bw2set
-	lat_r[a]=lat2set
-	return s,t,bw_r,lat_r,Requests
+##### parameters #####
+number_of_demands		= 3
+graph_type			= 'deight'
+bw_variants			= [1]
+lat_variants			= [10]
 
 ##### produce graph #####
-G=gc.make('eight')
+# produce graph and convert to digraph
+G=gc.make(graph_type)
 H=G.to_directed()
+
+# convert to gurobi graph
 H_nodes=H.nodes()
 H_nodes=[str(n) for n in H_nodes]
 H_edges=H.edges()
@@ -33,30 +32,24 @@ for e,w in H_edges:
 edges = tuplelist(H_edges)
 
 # create all demands
-number_of_demands=3
-bw_variants=[1]
-lat_variants=[10]
 d_list=[]
+for k in range(number_of_demands):
+	temp_dem = dem.demand(H.nodes(),bw_variants,lat_variants)
+	temp_dem.make_choice()
+	d_list.append(temp_dem)
+
+# convert to gurobi requests
 Requests=[]
 s={}
 t={}
 bw_r={}
 lat_r={}
-for k in range(number_of_demands):
-	temp_dem = dem.demand(H.nodes(),bw_variants,lat_variants)
-	temp_dem.make_choice()
-	d_list.append(temp_dem)
+for k in range(len(d_list)):
 	Requests.append(k)
-	s[k]=str(temp_dem.source)
-	t[k]=str(temp_dem.target)
-	bw_r[k]=temp_dem.bw
-	lat_r[k]=temp_dem.lat
-
-for r in Requests:
-	print 'Request: '+str(r)+' from '+str(s[r])+' to '+str(t[r])+' with bw_r '+str(bw_r[r])+' and lat_r '+str(lat_r[r])
-print '\n'
-
-
+	s[k]=str(d_list[k].source)
+	t[k]=str(d_list[k].target)
+	bw_r[k]=d_list[k].bw
+	lat_r[k]=d_list[k].lat
 
 ##### Create optimization model #####
 m = Model('sdan')
@@ -79,13 +72,11 @@ m.update()
 acceptance = quicksum(x[r] for r in Requests)
 m.setObjective(acceptance, GRB.MAXIMIZE)
 
-
 # subject to Nr 1
 for r in Requests:
 	outgoing=quicksum(P[r,e,w] for e,w in edges.select(s[r],'*'))
 	incoming=quicksum(P[r,e,w] for e,w in edges.select('*',s[r]))
         m.addConstr(x[r] == outgoing - incoming)
-
 
 # subject to Nr 2
 for r in Requests:
@@ -95,33 +86,45 @@ for r in Requests:
 			incoming=quicksum(P[r,e,w] for e,w in edges.select('*',k))
         		m.addConstr(0 == outgoing - incoming)
 
-
 # subject to Nr 3
 for e,w in edges:
 	m.addConstr(bw_e[e,w] >= quicksum(bw_r[r]*[P[r,e,w]][0] for r in Requests))
 
-
 # subject to Nr 4
 for r in Requests:
 	m.addConstr(lat_r[r] >= quicksum(lat_e[e,w]*[P[r,e,w]][0] for e,w in edges))
-
 
 ##### Compute optimal solution ##### 
 m.optimize()
 
 ##### Print solution ##### 
 
-# Check if all bw_e constraints are held
-all_bw_hold=True
-for e,w in edges:
-	for r in Requests: 
-		ca = bw_e[e,w]
-		pa = bw_r[r]*[P[r,e,w]][0].x
-		if not ca>=pa:
-			all_bw_hold=False
 print '\n%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%'
-print '\nAll bw constraints held: '+str(all_bw_hold)
-print('acceptance: %g \n' % acceptance.getValue())
+# Check if all bw_e constraints are held
+all_bw_held=True
+for e,w in edges:
+	if (bw_e[e,w] < quicksum(bw_r[r]*[P[r,e,w]][0].x for r in Requests)):
+		all_bw_hold=False
+
+# Check if all lat_e constraints are held
+all_lat_held=True
+for r in Requests:
+	if (lat_r[r] < quicksum(lat_e[e,w]*[P[r,e,w]][0].x for e,w in edges)):
+		all_lat_held=False
+
+print '\n'
+# Print requests
+for r in Requests:
+	print 'Request: '+str(r)+' from '+str(s[r])+' to '+str(t[r])+' with bw_r '+str(bw_r[r])+' and lat_r '+str(lat_r[r])
+
+print '\nAll bw constraints held: '+str(all_bw_held)
+print 'All lat constraints held: '+str(all_lat_held)
+accepted=acceptance.getValue()
+rejected=number_of_demands-accepted
+ratio=float(accepted)/float(number_of_demands)
+print '\nAccepted: '+str(accepted)
+print 'Rejected: '+str(rejected)
+print 'Ratio: '+str(ratio)+'\n'
 
 for v in m.getVars():
 	if v.varName.startswith('x'):
@@ -141,7 +144,7 @@ for r in Requests:
 			temp_path.append((from_node,to_node))
 	paths[r, s[r], t[r]]=temp_path
 
-print '\n'
+result=[]
 for r in Requests:
 	e_list=tuplelist(paths[r, s[r], t[r]])
 	pp=[]
@@ -151,8 +154,10 @@ for r in Requests:
 	for j in range(1,len(e_list)):
 		temp_edge= e_list.select(pp[j],'*')[0]
 		pp.append(temp_edge[1])
-	print 'Request: '+str(r)+' from '+str(s[r])+' to '+str(t[r])+' has path '+str(pp)
-
+	print 'Request: '+str(r)+' from '+str(s[r])+' to '+str(t[r])+' has path '+str([int(z) for z in pp])
+	result.append([int(z) for z in pp])
+print '\n'
+print result
 
 
 
